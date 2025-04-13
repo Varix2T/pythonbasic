388025d5fc8917325a4fa4dbbda734bbd7f8a42d
VariX2T <varivarit060412@gmail.com>
2025-04-13 20:47:09 +0900

    EP03 Commit

---
+++ EP03_forMoon.py
@@ -0,0 +1,18 @@
+#range 함수 for문
+# for num in range(1,101):
+#     print(num)
+#
+# print("end")
+#Ctrl+/로 코멘트(주석)화
+
+# #list for문
+# fruits = ["사과","바나나","파인애플"]
+#
+# for i in fruits:
+#     print(i)
+
+# dictionary for문
+# ("key": value)
+student_scores = {"철수":90,"영희":100,"민수":98}
+for name, score in student_scores.items():
+    print(name,score)
